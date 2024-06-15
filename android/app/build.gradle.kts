17ea2a8436fe09ab8a1b0208f443e932fe4a307b
a.savanovich <asavanster@gmail.com>
2024-06-15 17:10:34 +0300

    asset links

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ru.asavan.drixit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.2"
+        versionCode = 2
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
