efd175a487c4f0ebaba3435a123b5146f4036759
a.savanovich <asavanster@gmail.com>
2024-06-16 17:17:22 +0300

    big circles

--- tests/manual/main.js
+++ tests/manual/main.js
@@ -1,5 +1,4 @@
 function main() {
-    console.log("dada");
     const box = document.querySelector(".circle-wrapper");
     const N = 30;
     for (let i = 0; i < N; ++i) {
@@ -8,13 +7,26 @@ function main() {
         elem.style.setProperty("--path-pc", pathPercent);
         elem.classList.add("circle");
         if ((i+1)%5 === 0) {
+            elem.classList.add("big");
             const pileElem = document.createElement("div");
             pileElem.textContent = (i+1);
             pileElem.classList.add("card-count");
             elem.appendChild(pileElem);
         }
-        box.appendChild(elem);
 
+        if (i === 12) {
+            const anchorName = "--anchor-el";
+            // elem.style.setProperty("anchor-name", anchorName);
+        }
+
+        box.appendChild(elem);
+    }
+
+    for (const i of [4]) {
+        const pileElem = document.createElement("li");
+        pileElem.classList.add("anchored-notice");
+        pileElem.textContent = "sasha" + (i+1);
+        box.appendChild(pileElem);
     }
 };
 
