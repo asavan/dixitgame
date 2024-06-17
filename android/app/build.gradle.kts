caeb662303ffe7bc47526b5151da2c7d26fc885a
a.savanovich <asavanster@gmail.com>
2024-06-17 16:43:04 +0300

    0.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ru.asavan.drixit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.3"
+        versionCode = 3
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
