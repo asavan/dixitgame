7740dfd5ccbdadda90fca64da241dcc3c5910e3c
asavan <asavan@mail.ru>
2025-07-03 02:52:57 +0300

    fix

--- src/js/views/settings-form-btn.js
+++ src/js/views/settings-form-btn.js
@@ -5,13 +5,13 @@ export default function addSettingsButton(document, settings) {
     const controlPanel = controlPanelTmpl.content.cloneNode(true).firstElementChild;
     document.querySelector(".settings-cont").appendChild(controlPanel);
 
-    document.querySelector("#maximizeBtn").addEventListener("click", (e) => {
+    document.querySelector("#maximize-btn").addEventListener("click", (e) => {
         e.preventDefault();
         const controlPanel = document.querySelector("#control-panel");
         controlPanel.classList.remove("minimized");
         const formInstance = createBooleanForm(settings);
         formInstance.appendTo(".panel-content");
-        document.querySelector("#minimizeBtn").addEventListener("click", e => {
+        document.querySelector("#minimize-btn").addEventListener("click", e => {
             e.preventDefault();
             controlPanel.classList.add("minimized");
             formInstance.destroy();
